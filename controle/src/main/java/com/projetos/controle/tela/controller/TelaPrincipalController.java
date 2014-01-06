f134ef9953d9fa249cb722f3c01d1a73dfce4fbc
rfrade <rafaelfrade@live.com>
2014-01-05 22:46:32 -0200

    Criado menu da tela principal

--- controle/src/main/java/com/projetos/controle/tela/controller/TelaPrincipalController.java
+++ controle/src/main/java/com/projetos/controle/tela/controller/TelaPrincipalController.java
@@ -1,5 +1,8 @@
 package com.projetos.controle.tela.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
@@ -14,8 +17,12 @@ public class TelaPrincipalController extends AbstractController {
 	@Autowired
 	private ClienteController clienteController;
 
+	@FXML
+	private Pane panePrincipal;
+
 	public void exibirTelaClienteLista() {
 		clienteController.exibirTelaClienteLista();
+		/*panePrincipal.*/
 	}
 
 }
